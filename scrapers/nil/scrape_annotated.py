33038bab27db4270b7e25e864f5ac2b6f38e46c1
sbansi01 <113473608+sbansi01@users.noreply.github.com>
2023-04-05 18:30:51 +0000

    bot update

--- scrapers/nil/scrape_annotated.py
+++ scrapers/nil/scrape_annotated.py
@@ -12,49 +12,42 @@ json = response.json()
 # resource for parsing json keys: https://jsonlint.com/
 
 results = []
-# creates an empty list called players that we will use to store the data
+# creates an empty list called results that we will use to store the data
 
 big_ten_teams = ['Illinois Fighting Illini', 'Indiana Hoosiers', 'Iowa Hawkeyes', 'Maryland Terrapins', 'Michigan Wolverines', 'Michigan State Spartans', 'Minnesota Golden Gophers', 'Nebraska Cornhuskers', 'Northwestern Wildcats', 'Ohio State Buckeyes', 'Penn State Nittany Lions', 'Purdue Boilermakers', 'Rutgers Scarlet Knights', 'Wisconsin Badgers']
 # In Python, an equal sign assigns a variable. We've created a variable with all the Big Ten teams.
 
-players = json['list']
-# I think we're making a variable called player_data that gets the json version of everything that is under 'list' on On3NIL for the first player (0)
-
-for player_data in players:
-
-    # might need a statement here like: for player in players: (potential resource: https://www.w3schools.com/python/python_for_loops.asp)
-    # might need to put all of this in an if statement that says: if the team is in the big ten:
-    print(player_data)
+for player_data in json['list']:
     class_year = player_data['person']['classYear']
     class_rank = player_data['person']['classRank']
     first_name = player_data['person']['firstName']
     last_name = player_data['person']['lastName']
     if 'sport' in player_data and 'name' in player_data['sport']:
         sport = player_data['sport']['name']
-    elif player_data['rating'] is not None:
+    elif player_data.get('rating') and 'sport' in player_data['rating']:
         sport = player_data['rating']['sport']['name']
     else:
         sport = None
-    # https://www.w3schools.com/python/python_conditions.asp
-    if player_data['company'] is not None:
+    if 'company' in player_data and player_data['company']:
         company = player_data['company']['name']
+    elif 'collectiveGroup' in player_data and player_data['collectiveGroup']:
+        company = player_data['collectiveGroup']['name']
     else:
         company = None
-    if player_data['collectiveGroup'] is not None:
-        collective = player_data['collectiveGroup']['name']
-    else:
-        collective = None
-    # ideally, I think I would like this code to be "if there is something in 'company,' print the word "company," otherwise print the word "collective;" then a separate line of code would pull the name of the company or the collective
     date = player_data['date']
-    # is there a way to clean up this output?
     url = player_data['sourceUrl']
-    school = player_data['status']['committedAsset']['fullName']
-    # might need to put an else statement here that says: if the team is not in the big ten, then continue
+    if 'status' in player_data and player_data['status'] is not None and 'committedAsset' in player_data['status'] and player_data['status']['committedAsset'] is not None:
+        school = player_data['status']['committedAsset']['fullName']
+    else:
+        school = None
+# ideally, I think I would like this code to be "if there is something in 'company,' print the word "company," otherwise print the word "collective;" then a separate line of code would pull the name of the company or the collective
+# is there a way to clean up the date output?
 
-    results.append([date, first_name, last_name, school, sport, class_year, class_rank, company, collective, url])
+    if school in big_ten_teams:
+        results.append([date, first_name, last_name, school, sport, class_year, class_rank, company, url])
+    else:
+        continue
     # Here, we're saying take the dictionary I made above and append it to the empty list (players) we made earlier.
 
 print(results)
 # Output the players list
-
-# This is working for many players, but getting errors when there is no sport or when the player got a deal from a collective ('NoneType' object is not subscriptable). Also getting same error message for some players in other variables (e.g., school).
